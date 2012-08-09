d230a599272a187a2664842691f20fa79fbdf616
Samuel Gabrielsson <samuel.gabrielsson@ericsson.com>
2012-08-09 09:56:55 +0200

    Retab some files

--- lib/functions.sh
+++ lib/functions.sh
@@ -2,45 +2,45 @@
 # ARGS: CPUS
 function setup_environment()
 {
-	# Uset each variable and only keep he necessary ones.
-	unset $(/usr/bin/env | egrep '^(\w+)=(.*)$' | \
-		egrep -vw 'HOME|TERM|PATH|PWD|SHLVL|_' | /usr/bin/cut -d= -f1);
+    # Uset each variable and only keep he necessary ones.
+    unset $(/usr/bin/env | egrep '^(\w+)=(.*)$' | \
+        egrep -vw 'HOME|TERM|PATH|PWD|SHLVL|_' | /usr/bin/cut -d= -f1);
 
-	set +h
-	umask 022
+    set +h
+    umask 022
 
-	LC_ALL=POSIX
-	LFS_TGT=$(uname -m)-proximus-linux-gnu
-	PATH=$LFS$TOOLS/bin:/bin:/usr/bin
-	PS1='\u:\w\$ '
-	MAKEFLAGS="-j $CPUS"
+    LC_ALL=POSIX
+    LFS_TGT=$(uname -m)-proximus-linux-gnu
+    PATH=$LFS$TOOLS/bin:/bin:/usr/bin
+    PS1='\u:\w\$ '
+    MAKEFLAGS="-j $CPUS"
 
-	export LFS TOOLS LC_ALL LFS_TGT PATH PS1 MAKEFLAGS
+    export LFS TOOLS LC_ALL LFS_TGT PATH PS1 MAKEFLAGS
 }
 
 # Function with customized print.
 function print_me()
 {
-	printf "$(date +"%F-%T") %-*s %s" "$1" "$2"
+    printf "$(date +"%F-%T") %-*s %s" "$1" "$2"
 }
 
 # Function will print ok in color.
 function print_ok()
 {
-	printf '%b\n' "\e[1;32m[  OK  ]\e[m"
+    printf '%b\n' "\e[1;32m[  OK  ]\e[m"
 }
 
 # Function will print fail in color.
 function print_fail()
 {
-	printf '%b\n' "\e[1;31m[ FAIL ]\e[m"
-	echo "================================================================================"
-	echo "Dumping debug environement"
-	echo "================================================================================"
-	env
+    printf '%b\n' "\e[1;31m[ FAIL ]\e[m"
+    echo "================================================================================"
+    echo "Dumping debug environement"
+    echo "================================================================================"
+    env
 
-	echo "================================================================================"
-	echo "Dumping debug error logs"
-	echo "================================================================================"
-	tail -n 10 $LOGDIR/*.err
+    echo "================================================================================"
+    echo "Dumping debug error logs"
+    echo "================================================================================"
+    tail -n 10 $LOGDIR/*.err
 }
--- prox-dist.sh
+++ prox-dist.sh
@@ -8,9 +8,9 @@ CONFIG_FILE=$1
 DIR=$( cd "$( dirname "$0" )" && pwd )
 
 [ $# -eq 0 ] && {
-	echo "Example:"
-	echo "$0 conf/lfs-7.1-tools.cfg"
-       	exit 1;
+    echo "Example:"
+    echo "$0 conf/lfs-7.1-tools.cfg"
+        exit 1;
 }
 
 # Set the LFS variable
@@ -33,26 +33,26 @@ SOURCES="$LFS"/usr/src/sources
 o_IFS=$IFS
 IFS=$'\n'
 for line in `cat "$CONFIG_FILE"`; do
-	IFS=$o_IFS
-	export LOGDIR=""
-	# Remove comments starting with '#'
-	[[ "$line" = \#* ]] && continue
+    IFS=$o_IFS
+    export LOGDIR=""
+    # Remove comments starting with '#'
+    [[ "$line" = \#* ]] && continue
 
-	# Get the name of the script
-	script="${line##*/}"
-	# Set the log directory
-	LOGDIR="$SOURCES"/log/"$script" && mkdir -p "$LOGDIR" || exit 1
+    # Get the name of the script
+    script="${line##*/}"
+    # Set the log directory
+    LOGDIR="$SOURCES"/log/"$script" && mkdir -p "$LOGDIR" || exit 1
 
-	# Print the script
+    # Print the script
     echo "================================================================================"
     echo "Executing: $(eval echo $line)"
     echo "Log dir: ${LOGDIR}"
     echo "================================================================================"
 
-	# Run the build libraries
-	source "$DIR"/lib/build.sh
-	# Run the actual package build scipt
-	source $(eval echo "$line")
+    # Run the build libraries
+    source "$DIR"/lib/build.sh
+    # Run the actual package build scipt
+    source $(eval echo "$line")
 done
 IFS=$o_IFS
 echo "================================================================================"
