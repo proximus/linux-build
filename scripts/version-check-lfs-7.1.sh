8b7d1fcc8aadbd1c2c17340845c5476681ab9802
Samuel Gabrielsson <samuel.gabrielsson@gmail.com>
2013-03-28 18:05:08 +0100

    Remove trailing whitespace

--- scripts/version-check-lfs-7.1.sh
+++ scripts/version-check-lfs-7.1.sh
@@ -7,7 +7,7 @@ echo "/bin/sh -> `readlink -f /bin/sh`"
 echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
 bison --version | head -n1
 if [ -e /usr/bin/yacc ];
-  then echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`"; 
+  then echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
   else echo "yacc not found"; fi
 
 bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-
@@ -16,7 +16,7 @@ diff --version | head -n1
 find --version | head -n1
 gawk --version | head -n1
 if [ -e /usr/bin/awk ];
-  then echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`"; 
+  then echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
   else echo "awk not found"; fi
 
 gcc --version | head -n1
@@ -34,7 +34,7 @@ echo "Texinfo: `makeinfo --version | head -n1`"
 xz --version | head -n1
 
 echo 'main(){}' > dummy.c && gcc -o dummy dummy.c
-if [ -x dummy ] 
+if [ -x dummy ]
   then echo "gcc compilation OK";
   else echo "gcc compilation failed"; fi
 rm -f dummy.c dummy
